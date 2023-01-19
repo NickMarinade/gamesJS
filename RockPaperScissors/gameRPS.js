40c579ce44fb909f9e79609e222efbf7887bce69
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-19 10:46:56 +0100

    fix event of choosing sign
    adding data-sign, so when the sign is clicked, it can be picked up as 'humanPick' in code

--- RockPaperScissors/gameRPS.js
+++ RockPaperScissors/gameRPS.js
@@ -4,20 +4,22 @@ let getSigns = getMainDiv.querySelectorAll('.box');
 
 let signsArray = ['rock', 'paper', 'scissors'];
 
-let humanScore, machineScore;
-humanScore = 0;
+let humanScore;
+let machineScore;
+humanScore = machineScore = 0;
 
 let getHumanScore = getMainDiv.querySelector('.humanScore');
 let getMachineScore = getMainDiv.querySelector('.machineScore');
 
 let getFinalResult = getMainDiv.querySelector('.finalResult');
 
-let getChoicesText = getMainDiv.querySelectorAll('.choiceText');
+let getChoiceTexts = getMainDiv.querySelectorAll('.choiceText');
 let humanChoiceText = getMainDiv.querySelector('.choiceTextHuman');
 let machineChoiceText = getMainDiv.querySelector('.choiceTextMachine'); 
+let getPlayAgainButton = getMainDiv.querySelector('.button');
 
 for (let sign of getSigns) {
-    sign.addEventListener('click', makeMove, console.log('clicked'));
+    sign.addEventListener('click', makeMove);
 }
 
 function makeMove() {
@@ -54,9 +56,12 @@ function moveResult(human, machine) {
 
 
 function updateBoard(moveResult, humanPick, machinePick) {
-debugger;
-    humanChoiceText.textContent = humanPick;
-    machineChoiceText.textContent = machinePick;
+    for(let text of getChoiceTexts) {
+        text.classList.add('isVisible');
+    }
+
+     humanChoiceText.textContent = humanPick;
+     machineChoiceText.textContent = machinePick;
 
     moveResult == 'human' ? humanScore++ : null;
     moveResult == 'machine' ? machineScore++ : null;
