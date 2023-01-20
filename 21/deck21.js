4e469df19a436a9e7774b3fbfede21fe18d7dff6
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-20 12:19:32 +0100

    link cards images to the array

--- 21/deck21.js
+++ 21/deck21.js
@@ -3,28 +3,28 @@ let deckOfCards = [
         'card':'2',
         'value': 2,
         'suit': 'club',
-        'image': ''
+        'image': 'images/2_of_clubs.png'
      },
 
      {
         'card':'2',
         'value': 2,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/2_of_diamonds.png'
      },
 
      {
         'card':'2',
         'value': 2,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/2_of_hearts.png'
      },
 
      {
         'card':'2',
         'value': 2,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/2_of_spades.png'
      },
 
 
@@ -32,224 +32,224 @@ let deckOfCards = [
         'card':'3',
         'value': 3,
         'suit': 'club',
-        'image': ''
+        'image': 'images/3_of_clubs.png'
      },
 
      {
         'card':'3',
         'value': 3,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/3_of_diamonds.png'
      },
 
      {
         'card':'3',
         'value': 3,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/3_of_hearts.png'
      },
 
      {
         'card':'3',
         'value': 3,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/3_of_spades.png'
      },
 
     {
         'card':'4',
         'value': 4,
         'suit': 'club',
-        'image': ''
+        'image': 'images/4_of_clubs.png'
      },
 
      {
         'card':'4',
         'value': 4,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/4_of_diamonds.png'
      },
 
      {
         'card':'4',
         'value': 4,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/4_of_hearts.png'
      },
 
      {
         'card':'4',
         'value': 4,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/4_of_spades.png'
      },
 
     {
         'card':'5',
         'value': 5,
         'suit': 'club',
-        'image': ''
+        'image': 'images/5_of_clubs.png'
      },
 
      {
         'card':'5',
         'value': 5,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/5_of_diamonds.png'
      },
 
      {
         'card':'5',
         'value': 5,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/5_of_hearts.png'
      },
 
      {
         'card':'5',
         'value': 5,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/5_of_spades.png'
      },
 
     {
         'card':'6',
         'value': 6,
         'suit': 'club',
-        'image': ''
+        'image': 'images/6_of_clubs.png'
      },
 
      {
         'card':'6',
         'value': 6,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/6_of_diamonds.png'
      },
 
      {
         'card':'6',
         'value': 6,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/6_of_hearts.png'
      },
 
      {
         'card':'6',
         'value': 6,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/6_of_spades.png'
      },
 
     {
         'card':'7',
         'value': 7,
         'suit': 'club',
-        'image': ''
+        'image': 'images/7_of_clubs.png'
      },
 
      {
         'card':'7',
         'value': 7,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/7_of_diamonds.png'
      },
 
      {
         'card':'7',
         'value': 7,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/7_of_hearts.png'
      },
 
      {
         'card':'7',
         'value': 7,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/7_of_spades.png'
      },
 
     {
         'card':'8',
         'value': 8,
         'suit': 'club',
-        'image': ''
+        'image': 'images/8_of_clubs.png'
      },
 
      {
         'card':'8',
         'value': 8,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/8_of_diamonds.png'
      },
 
      {
         'card':'8',
         'value': 8,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/8_of_hearts.png'
      },
 
      {
         'card':'8',
         'value': 8,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/8_of_spades.png'
      },
 
     {
         'card':'9',
         'value': 9,
         'suit': 'club',
-        'image': ''
+        'image': 'images/9_of_clubs.png'
      },
 
      {
         'card':'9',
         'value': 9,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/9_of_diamonds.png'
      },
 
      {
         'card':'9',
         'value': 9,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/9_of_hearts.png'
      },
 
      {
         'card':'9',
         'value': 9,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/9_of_spades.png'
      },
 
     {
         'card':'10',
         'value': 10,
         'suit': 'club',
-        'image': ''
+        'image': 'images/10_of_clubs.png'
      },
 
      {
         'card':'10',
         'value': 10,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/10_of_diamonds.png'
      },
 
      {
         'card':'10',
         'value': 10,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/10_of_hearts.png'
      },
 
      {
         'card':'10',
         'value': 10,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/10_of_spades.png'
      },
 
      {
@@ -257,101 +257,101 @@ let deckOfCards = [
         'value1': 1,
         'value2': 11,
         'suit': 'clubs',
-        'image': ''
+        'image': 'images/ace_of_clubs.png'
      },
      {
         'card':'ace',
         'value1': 1,
         'value2': 11,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/ace_of_diamonds.png'
      },
      {
         'card':'ace',
         'value1': 1,
         'value2': 11,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/ace_of_hearts.png'
      },
      {
         'card':'ace',
         'value1': 1,
         'value2': 11,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/ace_of_spades.png'
      },
 
      {
         'card':'jack',
         'value': 10,
         'suit': 'clubs',
-        'image': ''
+        'image': 'images/jack_of_clubs2.png'
      },
      {
         'card':'jack',
         'value': 10,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/jack_of_diamonds2.png'
      },
      {
         'card':'jack',
         'value': 10,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/jack_of_hearts2.png'
      },
      {
         'card':'jack',
         'value': 10,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/jack_of_spades2.png'
      },
      {
         'card':'king',
         'value': 10,
         'suit': 'clubs',
-        'image': ''
+        'image': 'images/king_of_clubs2.png'
      },
      {
         'card':'king',
         'value': 10,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/king_of_diamonds2.png'
      },
      {
         'card':'king',
         'value': 10,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/king_of_hearts2.png'
      },
      {
         'card':'king',
         'value': 10,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/king_of_spades2.png'
      },
      {
         'card':'queen',
         'value': 10,
         'suit': 'clubs',
-        'image': ''
+        'image': 'images/queen_of_clubs2.png'
      },
      {
         'card':'queen',
         'value': 10,
         'suit': 'diamonds',
-        'image': ''
+        'image': 'images/queen_of_diamonds2.png'
      },
      {
         'card':'queen',
         'value': 10,
         'suit': 'hearts',
-        'image': ''
+        'image': 'images/queen_of_hearts2.png'
      },
      {
         'card':'queen',
         'value': 10,
         'suit': 'spades',
-        'image': ''
+        'image': 'images/queen_of_spades2.png'
      },
 
 ]
--- 21/script21.js
+++ 21/script21.js
@@ -1,1 +1,7 @@
-console.log(deckOfCards);
+let image = document.createElement('img');
+    image.src = deckOfCards[0].image;
+    document.querySelector('.dealerHand').append(image);
+
+let imageBack = document.createElement('img');
+    imageBack.src = 'images/back.png'
+    document.querySelector('.dealerHand').append(imageBack);
