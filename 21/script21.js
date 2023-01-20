bdce377da450d106204b29bb3e23c20dcb8f488f
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-20 15:01:57 +0100

    expand 'deal' button logic
    adding point to the screen

--- 21/script21.js
+++ 21/script21.js
@@ -37,7 +37,42 @@ dealButton.addEventListener('click', () => {
         let index2 = deck.findIndex(d => d === randomCard2);
         deck.splice(index2, 1);
         dealerArr.push(randomCard2);
-        console.log(dealerArr, playerArr);
     }
+
+    if (dealerArr.some(card => card.card === 'ace' )) {
+        if(dealerArr[0].card === 'ace' && dealerArr[1].card !== 'ace') {
+            dlrPoint = dealerArr[0].value2 + dealerArr[1].value;
+        
+        } else if (dealerArr[0].card !== 'ace' && dealerArr[1].card === 'ace') {
+            dlrPoint = dealerArr[0].value + dealerArr[1].value2;
+        
+        } else {
+            dlrPoint = dealerArr[0].value1 + dealerArr[1].value2;
+        }
+
+    } else {
+        dlrPoint = dealerArr[0].value + dealerArr[1].value
+    } 
+
+    dealerPoints.innerHTML = dlrPoint;
+
+    if (playerArr.some(card => card.card === 'ace' )) {
+        if(playerArr[0].card === 'ace' && playerArr[1].card !== 'ace') {
+            plrPoint = playerArr[0].value2 + playerArr[1].value;
+        
+        } else if (playerArr[0].card !== 'ace' && playerArr[1].card === 'ace') {
+            plrPoint = playerArr[0].value + playerArr[1].value2;
+        
+        } else {
+            plrPoint = playerArr[0].value1 + playerArr[1].value2;
+        }
+
+    } else {
+        plrPoint = playerArr[0].value + playerArr[1].value
+    } 
+
+    playerPoints.innerHTML = plrPoint;
+
+
 })
 
